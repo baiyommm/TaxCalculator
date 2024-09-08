5a2b719d0645daafb83e2ea55d2448a685fd07c0
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-08 01:07:32 -0700

    Initial Commit

---
+++ TaxCalculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TaxCalculator
+//
+//  Created by Chatchapong Thuaypha on 9/8/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
