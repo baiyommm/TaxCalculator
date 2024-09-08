5abfdbd8044f74276d07641e485662e1fbb4cbec
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-08 01:29:30 -0700

    Included the variables and assigned the initialization (formula) to calculate the taxes.

--- TaxCalculator/main.cpp
+++ TaxCalculator/main.cpp
@@ -1,14 +1,17 @@
-//
-//  main.cpp
-//  TaxCalculator
-//
-//  Created by Chatchapong Thuaypha on 9/8/24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // Variables
+    double revenue = 95000.0;      // Revenue
+    double stateTaxRate = 0.04;    // State Tax Rate (4%)
+    double countyTaxRate = 0.02;   // County Tax Rate (2%)
+    
+    // Calculate Taxes
+    double stateTax = revenue * stateTaxRate;   // Calculate state tax
+    double countyTax = revenue * countyTaxRate; // Calculate county tax
+    double totalTax = stateTax + countyTax;     // Total tax is the sum of state and county tax
+
     return 0;
 }
